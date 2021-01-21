71d1362c3b80805ad591fabcdfcc8791a4899975
Lakhte <lakhteagha@gmail.com>
2021-01-21 16:02:44 -0500

    finished my_uniq

--- tdd_proj/lib/exercises.rb
+++ tdd_proj/lib/exercises.rb
@@ -1,3 +1,7 @@
 def my_uniq(arr)
-
+    arr_2 = []
+    arr.each { |el| arr_2 << el if !arr_2.include?(el) } 
+    arr_2
 end
+
+def 
--- tdd_proj/spec/exercises_spec.rb
+++ tdd_proj/spec/exercises_spec.rb
@@ -2,9 +2,37 @@ require 'rspec'
 require 'exercises'
 
 describe "#my_uniq" do
-  it "accepts an array as an argument" do
-    a = [1,2,3]
-    expect(my_uniq(a)).with(instance_of(Array))
+  let(:arr) { [1, 2, 1, 3, 3] }
 
+  it "removes duplicates" do
+    expect(my_uniq(arr)).to eq([1,2,3])
+  end
+
+  it "does not modify original array" do
+    expect(my_uniq(arr)).to_not be(arr)
   end
 end
+
+describe "#to_sum" do 
+  let(:arr) { [-1, 0, 2, -2, 1] }
+
+  it "return all zero sum pairs" do
+
+  end
+
+  it " " do
+    
+  end
+end
+# RSpec.describe "Constraining a message expectation using with" do
+#   let(:dbl) { double }
+#   before { expect(dbl).to receive(:foo).with(1, anything, /bar/) }
+
+#   it "passes when the args match" do
+#     dbl.foo(1, nil, "barn")
+#   end
+
+#   it "fails when the args do not match" do
+#     dbl.foo(1, nil, "other")
+#   end
+# end
